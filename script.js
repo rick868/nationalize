127b76528bbdfecdb0d6c6d17b4fed770f5e5752
Your Name <your.email@example.com>
2025-05-27 19:37:34 +0300

    added new changes to the site, background color, and improved functionalities

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const nameInput = document.getElementById('name');
-const resultsDiv = document.getElementById('results');
-const submitButton = document.getElementById('submit-btn');
-const errorMessage = document.getElementById('error-message');
-const nameForm = document.getElementById('name-form');
-
-nameForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const name = nameInput.value();
+const RequestBtn = document.getElementById("submit-btn");
+const resultsSection = document.getElementById("results-section");
+RequestBtn.addEventListener("click", async function (e) {
+  e.preventDefault();
+  RequestBtn.textContent = "Loading...";
+  RequestBtn.disabled = true;
+  const userInput = document.getElementById("user-input").value;
+  if (userInput === "") {
+    resultsSection.textContent = "Please enter your full name";
+    return;
+  }
+  try {
+    const responseDisplay = await fetch(
+      `https://api.nationalize.io/?name=${userInput}`
+    );
+    const result = await responseDisplay.json();
+    console.log(result);
     
+    const { name, country } = result;
+    if(country.length>0){
+
+    resultsSection.innerHTML = `<p>${name} is form ${country[0].country_id} with ${ country[0].probability * 100 }% certainty</p>`;
+}
+  } catch (error) {
+    resultsSection.textContent =
+      "An error occurred while fetching data. Please try again.";
+    return;
+  }
+  console.log(result);
+  RequestBtn.removeAttribute("disabled");
+  RequestBtn.innerHTML = `<p>Search</p>`;
+});
